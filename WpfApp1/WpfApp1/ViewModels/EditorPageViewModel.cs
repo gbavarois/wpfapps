4e0748be8153b04a8c52d111866ecc98f8373a6b
N <N@A520M-HDV>
2026-03-23 03:22:21 +0900

    とりあえずプッシュして同期。

---
+++ WpfApp1/WpfApp1/ViewModels/EditorPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    public partial class EditorPageViewModel : ObservableObject
+    {
+        private readonly MainViewModel _main;
+
+        [ObservableProperty] private string _pageName = "新規ページ";
+
+        // このページに配置されているRAMのリスト
+        public ObservableCollection<RamItemViewModel> PlacedRams { get; } = new();
+
+        // このページで選択されているRAM
+        [ObservableProperty] private RamItemViewModel? _selectedRam;
+
+        public EditorPageViewModel(MainViewModel main)
+        {
+            _main = main;
+        }
+
+        // 保存用データ（DTO）への変換
+        public EditorSaveData CreateSaveData(System.Collections.Generic.List<string> lines, System.Collections.Generic.List<TextColorInfo> colors)
+        {
+            var data = new EditorSaveData { Lines = lines, Colors = colors };
+            foreach (var ramVM in PlacedRams)
+            {
+                data.Rams.Add(ramVM.GetModel());
+            }
+            return data;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/ViewModels/RamItemViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    public partial class RamItemViewModel : ObservableObject
+    {
+        private readonly RamSaveData _model; // 保存対象のピュアなデータ
+        private readonly MainViewModel _main; // マスター参照用
+
+        public RamItemViewModel(RamSaveData model, MainViewModel main)
+        {
+            _model = model;
+            _main = main;
+        }
+
+        // 保存データ（Model）をそのまま取得したい時用
+        public RamSaveData GetModel() => _model;
+
+        // Modelの値をプロパティとして公開（変更されたらModelも書き換える）
+        public int Row
+        {
+            get => _model.Row;
+            set { if (SetProperty(_model.Row, value, _model, (m, v) => m.Row = v)) OnPropertyChanged(); }
+        }
+
+        public int Column
+        {
+            get => _model.Column;
+            set { if (SetProperty(_model.Column, value, _model, (m, v) => m.Column = v)) OnPropertyChanged(); }
+        }
+
+        public string FormatId
+        {
+            get => _model.FormatId;
+            set
+            {
+                if (SetProperty(_model.FormatId, value, _model, (m, v) => m.FormatId = v))
+                {
+                    OnPropertyChanged(nameof(Format));
+                    OnPropertyChanged(nameof(Length));
+                }
+            }
+        }
+
+        public string Symbol => _model.Symbol;
+
+        // --- 参照・計算プロパティ（Modelは持たず、ViewModelが解決する） ---
+
+        public RamCatalog? Catalog => _main.AllRamCatalogs.FirstOrDefault(c => c.Symbol == Symbol);
+
+        public FormatData? Format => _main.FormatList.FirstOrDefault(f => f.Id == FormatId);
+
+        public string Data => Catalog?.Data ?? "(不明)";
+
+        public int Length => Format?.Length ?? 8;
+
+        public string ComputedAddress
+        {
+            get
+            {
+                if (int.TryParse(Catalog?.Address, out var baseAddr))
+                    return (baseAddr + _model.Offset).ToString();
+                return Catalog?.Address ?? "";
+            }
+        }
+    }
+}
